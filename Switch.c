f7faca918892e6a01a0cc1d3f15e6d757c57040d
Madhu <104384319+106121071@users.noreply.github.com>
2022-11-18 03:13:15 +0530

    update switch.c

--- Switch.c
+++ Switch.c
@@ -5,28 +5,28 @@ Specafication- */
 #include<stdio.h>
 #include<stdlib.h>
 //main function
-main() {
+void main() {
 	//Variable Decleration
-	int choice=0;
-	double r = 0,circle=0,side=0,square=0;
+	int choice;
+	float r ,circle,side,square;
 	while(choice != 3)
 	{
 	printf("Press 1 for area of circle\n");
 	printf("Press 2 for area of square\n");
 	printf("Press 3 to CY@\n");
-	scanf_s("%i", &choice);
+	scanf("%d", &choice);
 	switch (choice) {
 	case 1:
 		printf("Enter the radius.\n\n");
-		scanf_s("%lf", &r);
+		scanf("%f", &r);
 		circle = 3.14*r*r;
-		printf("Area of circle is %.2lf\n", circle);
+		printf("Area of circle is %f\n", circle);
 		break;
 	case 2:
 		printf("Enter a side.\n");
-		scanf_s("%lf", &side);
+		scanf("%f", &side);
 		square = side*side;
-		printf("Area of square is %.2lf\n\n", square);
+		printf("Area of square is %f\n", square);
 
 		break;
 	case 3:
@@ -55,37 +55,41 @@ case 2 ask for side of square, calculate area of square, display area of square*
 #include<stdio.h>
 #include<stdlib.h>
 //main function
-main() {
+void main() {
 	//Variable Decleration
 	int choice = 0;
-	double r = 0, circle = 0, side = 0, square = 0;
+	float r = 0, circle = 0, side = 0, square = 0;
 	
 	{
+	    while(choice!=3){
 		printf("Press 1 for area of circle\n");
 		printf("Press 2 for area of square\n");
-		
-		scanf_s("%i", &choice);
+		printf("Press 3 to exit\n");
+		scanf("%d", &choice);
 		switch (choice) {
 		case 1:
-			printf("Enter the radius.\n\n");
-			scanf_s("%lf", &r);
+			printf("Enter the radius.\n");
+			scanf("%f", &r);
 			circle = 3.14*r*r;
-			printf("Area of circle is %.2lf\n", circle);
+			printf("Area of circle is %f\n", circle);
 			break;
 		case 2:
 			printf("Enter a side.\n");
-			scanf_s("%lf", &side);
+			scanf("%f", &side);
 			square = side*side;
-			printf("Area of square is %.2lf\n\n", square);
+			printf("Area of square is %f\n", square);
+			break;
+    	case 3:
+    	   printf("program terminated");
 			break;
 		default:
 			printf("you entered incorrect choice.\n");
 			break;
 		}
+		}
 	}
 	//End main
 	system("pause");
 }
-
 
 	
